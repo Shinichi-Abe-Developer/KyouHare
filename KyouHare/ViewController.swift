5fdb6757a375b23b9b222b33d21eae1e1d95ea5d
Shinichi Abe <asas025@icloud.com>
2023-02-01 00:28:22 +0900

    Initial Commit

---
+++ KyouHare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KyouHare
+//
+//  Created by Shinichi Abe on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
